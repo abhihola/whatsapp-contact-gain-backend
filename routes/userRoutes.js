0998b344e0af5a74abc2219915b04d911a141400
ABIOLA ELIJAH ALALADE <72648063+abhihola@users.noreply.github.com>
2025-03-11 23:16:39 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,23 +2,12 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
-// Add user
 router.post('/add', async (req, res) => {
   try {
-    const { name, whatsappNumber, email } = req.body;
-    const user = new User({ name, whatsappNumber, email });
+    const { name, whatsappNumber, email, language } = req.body;
+    const user = new User({ name, whatsappNumber, email, language });
     await user.save();
     res.status(201).json({ message: 'User added successfully' });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-// Get all users
-router.get('/', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
