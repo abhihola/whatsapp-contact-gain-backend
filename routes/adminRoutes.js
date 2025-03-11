7dccb62888915734f625b0239b7c275916a3680a
ABIOLA ELIJAH ALALADE <72648063+abhihola@users.noreply.github.com>
2025-03-11 22:59:44 +0100

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const AdminSettings = require('../models/AdminSettings');
 
+// Get current settings
+router.get('/settings', async (req, res) => {
+  try {
+    const settings = await AdminSettings.findOne();
+    res.json(settings);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 // Update default message
 router.post('/update-message', async (req, res) => {
   try {
