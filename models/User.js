42e2e8216ac45c7f6862a265a2d6259070a7b255
ABIOLA ELIJAH ALALADE <72648063+abhihola@users.noreply.github.com>
2025-03-11 23:54:44 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
   name: String,
   whatsappNumber: String,
   email: String,
+  language: { type: String, default: 'en' } // Default language is English
 });
 
 module.exports = mongoose.model('User', UserSchema);
