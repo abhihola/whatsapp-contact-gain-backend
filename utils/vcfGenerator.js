0cf9f3d9e4e83456481cf2b652a9a671381ddda9
ABIOLA ELIJAH ALALADE <72648063+abhihola@users.noreply.github.com>
2025-03-11 23:29:49 +0100

    Update vcfGenerator.js

--- utils/vcfGenerator.js
+++ utils/vcfGenerator.js
@@ -5,16 +5,16 @@ const generateVCF = async () => {
   try {
     const users = await User.find();
     let vcfContent = '';
-    
+
     users.forEach(user => {
       vcfContent += `BEGIN:VCARD\nVERSION:3.0\nFN:${user.name}\nTEL:${user.whatsappNumber}\nEMAIL:${user.email}\nEND:VCARD\n`;
     });
-    
+
     const filePath = './contacts.vcf';
     fs.writeFileSync(filePath, vcfContent);
     return filePath;
   } catch (error) {
-    console.error('Error generating VCF:', error);
+    console.error('❌ Error generating VCF:', error);
     return null;
   }
 };
