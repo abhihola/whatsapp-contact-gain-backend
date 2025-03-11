09f6efaf811c27721cd5a01c1b7ec732fb2606d6
ABIOLA ELIJAH ALALADE <72648063+abhihola@users.noreply.github.com>
2025-03-11 21:59:45 +0100

    Create vcfGenerator.js

---
+++ backend/utils/vcfGenerator.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const User = require('../models/User');
+
+const generateVCF = async () => {
+  try {
+    const users = await User.find();
+    let vcfContent = '';
+    
+    users.forEach(user => {
+      vcfContent += `BEGIN:VCARD\nVERSION:3.0\nFN:${user.name}\nTEL:${user.whatsappNumber}\nEMAIL:${user.email}\nEND:VCARD\n`;
+    });
+    
+    const filePath = './contacts.vcf';
+    fs.writeFileSync(filePath, vcfContent);
+    return filePath;
+  } catch (error) {
+    console.error('Error generating VCF:', error);
+    return null;
+  }
+};
+
+module.exports = { generateVCF };
